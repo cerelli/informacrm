35c2bd3bb489d9f007d8006f56de7d730ddc52f0
Marco Mapelli <cerelli.mm@gmail.com>
2017-09-25 15:47:19 +0200

    Change name of events table

--- database/seeds/AddDummyEvent.php
+++ database/seeds/AddDummyEvent.php
@@ -165,7 +165,7 @@ class AddDummyEvent extends Seeder
          ['title'=>'Super Event', 'start_date'=>'2017-09-23', 'end_date'=>'2017-09-24'],
          ['title'=>'wtf event', 'start_date'=>'2017-09-19', 'end_date'=>'2017-09-27'],
         ];
-        \DB::table('events')->insert($data);
+        \DB::table('inf_events')->insert($data);
 
 
     }
