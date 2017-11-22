0f686b92762d6a9e3186e1c76e70a8b76eaefd1b
Marco Mapelli <cerelli.mm@gmail.com>
2017-11-22 12:11:01 +0100

    Base language

---
+++ resources/lang/vendor/backpack/it/base.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => 'Le registrazioni sono chiuse.',
+    'first_page_you_see'   => 'La prima pagina che vedi dopo il login',
+    'login_status'         => 'Stato autenticazione',
+    'logged_in'            => 'Sei autenticato!',
+    'toggle_navigation'    => 'Apri/chiudi navigazione',
+    'administration'       => 'AMMINISTRAZIONE',
+    'user'                 => 'UTENTE',
+    'logout'               => 'Esci',
+    'login'                => 'Accedi',
+    'register'             => 'Registrati',
+    'name'                 => 'Nome',
+    'email_address'        => 'Indirizzo E-mail',
+    'password'             => 'Password',
+    'confirm_password'     => 'Conferma Password',
+    'remember_me'          => 'Ricordami',
+    'forgot_your_password' => 'Hai dimenticato la password?',
+    'reset_password'       => 'Reimposta password',
+    'send_reset_link'      => 'Invia link di reset',
+    'click_here_to_reset'  => 'Clicca qui per reimpostare la tua password',
+    'unauthorized'         => 'Non autorizzato.',
+    'dashboard'            => 'Cruscotto',
+    'handcrafted_by'       => 'Realizzato da',
+    'powered_by'           => 'Creato con',
+    'old_password'           => 'Vecchia Password',
+    'new_password'           => 'Nuova Password',
+    'change_password'        => 'Cambia Password',
+    'my_account'             => 'Profilo',
+    'update_account_info'    => 'Aggiorna Info Utente',
+    'save'                   => 'Salva',
+    'cancel'                 => 'Annulla',
+    'error'                  => 'Errore',
+    'success'                => 'Successo',
+    'old_password_incorrect' => 'La vecchia password non è corretta.',
+    'password_dont_match'    => 'le Passwords non coincidono.',
+    'password_empty'         => 'Assicurati di avere compilato entrambi i campo della password.',
+    'password_updated'       => 'Password aggiornata.',
+    'account_updated'        => 'Utente aggiornato con successo.',
+    'unknown_error'          => 'Errore non conosciuto. Per favore riprovare.',
+    'error_saving'           => 'Errore nel salvataggio. Per favore riprovare.',
+];
